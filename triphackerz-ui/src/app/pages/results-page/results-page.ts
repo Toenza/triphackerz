180ddcb792d1cac6c98fcb7ceb091e46ae435916
jonas.schulz (e532311) <9671803+jonasch@users.noreply.github.com>
2025-06-24 20:43:48 +0200

    define response signal

--- triphackerz-ui/src/app/pages/results-page/results-page.ts
+++ triphackerz-ui/src/app/pages/results-page/results-page.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
-import {MapComponent} from "../../map/map.component";
+import {Component, signal, WritableSignal} from '@angular/core';
+import {MapComponent} from '../../map/map.component';
 import {Location} from '../../models/location.model';
+import {ActivityRecommendationResponseItem} from '../../service/trip-search.service';
 
 @Component({
   selector: 'app-results-page',
@@ -12,12 +13,14 @@ import {Location} from '../../models/location.model';
 })
 export class ResultsPage {
 
-  // TODO here we have to set the start location to set the marker on the map
-  startLocation: Location = {latitude: 46.949026808315736, longitude: 7.439949741053424}; // default start location
-  // TODO here we have to set all the destinations to set the marker on the map
-  destinations: Location[] = [
-    {latitude: 46.94816653207459, longitude: 7.459474396895817}, // default value
-    {latitude: 46.9429048241674, longitude: 7.443830980779374} // default value
-  ];
+    // TODO here we have to set the start location to set the marker on the map
+    startLocation: Location = {latitude: 46.949026808315736, longitude: 7.439949741053424}; // default start location
+    // TODO here we have to set all the destinations to set the marker on the map
+    destinations: Location[] = [
+        {latitude: 46.94816653207459, longitude: 7.459474396895817}, // default value
+        {latitude: 46.9429048241674, longitude: 7.443830980779374} // default value
+    ];
+
+    results: WritableSignal<ActivityRecommendationResponseItem[]> = signal([]);
 
 }
