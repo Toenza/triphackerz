186f2c1d5d3ee8b5234b71d7197bf4da75dca901
e553102 <jerome.schaub@sbb.ch>
2025-06-24 09:56:06 +0200

    initial backend setup

---
+++ triphackerz-backend/src/main/java/ch/sbb/triphackerzbackend/TriphackerzBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.sbb.triphackerzbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriphackerzBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriphackerzBackendApplication.class, args);
+    }
+
+}
---
+++ triphackerz-backend/src/test/java/ch/sbb/triphackerzbackend/TriphackerzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.sbb.triphackerzbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriphackerzBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
