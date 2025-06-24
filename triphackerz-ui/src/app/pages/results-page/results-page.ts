d601a4706b568d91bc015eb3eef8e88f410ba45a
jonas.schulz (e532311) <9671803+jonasch@users.noreply.github.com>
2025-06-24 21:28:54 +0200

    show some ugly results

--- triphackerz-ui/src/app/pages/results-page/results-page.ts
+++ triphackerz-ui/src/app/pages/results-page/results-page.ts
@@ -1,29 +1,30 @@
 import {Component, inject, signal, WritableSignal} from '@angular/core';
-import {MapComponent} from "../../map/map.component";
+import {MapComponent} from '../../map/map.component';
 import {Location} from '../../models/location.model';
-import {ActivatedRoute, Router} from '@angular/router';
 import {ActivityRecommendationResponseItem, TripSearchService} from '../../service/trip-search.service';
-import {Observable} from 'rxjs';
+import {MatCardModule} from '@angular/material/card';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-results-page',
-  imports: [
-    MapComponent,
-  ],
-  templateUrl: './results-page.html',
-  styleUrl: './results-page.scss'
+    selector: 'app-results-page',
+    imports: [
+        MapComponent,
+        MatCardModule
+    ],
+    templateUrl: './results-page.html',
+    styleUrl: './results-page.scss'
 })
 export class ResultsPage {
-  private searchTripService = inject(TripSearchService);
+    private searchTripService = inject(TripSearchService);
 
-  constructor(private _activatedRoute: ActivatedRoute, private _router: Router) {
-    _activatedRoute.queryParams.subscribe(
-      params => {
-        console.log('queryParams', params);
-        this.searchTripService.searchTrip(params['lng'], params['lat'], 60, ['mountain biking', 'schwimmen'])
-          .subscribe(res => this.results.set(res));
-      });
-  }
+    constructor(private _activatedRoute: ActivatedRoute) {
+        this._activatedRoute.queryParams.subscribe(
+            params => {
+                console.log('queryParams', params);
+                this.searchTripService.searchTrip(params['lng'], params['lat'], 60, ['in einem see oder fluss schwimmen'])
+                    .subscribe(res => this.results.set(res));
+            });
+    }
 
     // TODO here we have to set the start location to set the marker on the map
     startLocation: Location = {latitude: 47.40264826377085, longitude: 8.49324703902625}; // default start location
@@ -31,7 +32,7 @@ export class ResultsPage {
     destinations: Location[] = [
         {latitude: 46.94816653207459, longitude: 7.459474396895817}, // default value
         {latitude: 46.9429048241674, longitude: 7.643830980779374}, // default value
-    {latitude: 46.8429048241674, longitude: 7.244830980779374}, // default value
+        {latitude: 46.8429048241674, longitude: 7.244830980779374} // default value
     ];
 
     results: WritableSignal<ActivityRecommendationResponseItem[]> = signal([]);
