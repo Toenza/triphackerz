aa5e8b869dfd7bc9d8c8a26dc174b9fea845c45f
e553102 <jerome.schaub@sbb.ch>
2025-06-24 14:20:30 +0200

    filter station names; only use parent stations and remove duplicates ("Zürich, blabla", "Zürich, asd" and so on)

--- triphackerz-backend/src/main/java/ch/sbb/triphackerzbackend/service/stationsearch/StationSearchService.java
+++ triphackerz-backend/src/main/java/ch/sbb/triphackerzbackend/service/stationsearch/StationSearchService.java
@@ -87,8 +87,9 @@ public class StationSearchService {
     }
 
     private List<Station> mapToStations(List<List<String>> records) {
-        return records.stream()
+        List<Station> parentStations = records.stream()
                 .filter(record -> !record.get(1).equals("stop_name"))
+                .filter(record -> record.getFirst().startsWith("Parent"))
                 .map(record -> new Station(
                         record.getFirst(), // id
                         record.get(1), // name
@@ -96,5 +97,60 @@ public class StationSearchService {
                         Double.valueOf(record.get(3)) // longitude
                 )).toList();
 
+        return filterStations(parentStations);
+    }
+
+    // Filter stations to remove duplicates (station names with ",") and average coordinates
+    private List<Station> filterStations(List<Station> stations) {
+        HashMap<String, List<Station>> clusteredParentStations = new HashMap<>();
+
+        for (Station station : stations) {
+            String name = station.getName();
+            int commaIndex = station.getName().indexOf(",");
+            if (commaIndex != -1) {
+                name = name.substring(0, commaIndex).trim();
+            }
+
+            if (clusteredParentStations.containsKey(name)) {
+                clusteredParentStations.get(name).add(station);
+            } else {
+                List<Station> list = new ArrayList<>();
+                list.add(station);
+                clusteredParentStations.put(name, list);
+            }
+        }
+
+        List<Station> filteredParentStations = new ArrayList<>();
+
+        for (String stationName : clusteredParentStations.keySet()) {
+            List<Station> clusteredStations = clusteredParentStations.get(stationName);
+            boolean containsExactStationName = clusteredStations.stream()
+                    .anyMatch(station -> station.getName().equals(stationName));
+            if (clusteredStations.size() > 1 && !containsExactStationName) {
+                double avgLatitude = clusteredStations.stream()
+                        .mapToDouble(Station::getLatitude)
+                        .average()
+                        .orElse(0.0);
+                double avgLongitude = clusteredStations.stream()
+                        .mapToDouble(Station::getLongitude)
+                        .average()
+                        .orElse(0.0);
+                filteredParentStations.add(new Station(
+                        clusteredStations.getFirst().getId(), // use the first station's ID
+                        stationName, // use the name without duplicates
+                        avgLatitude,
+                        avgLongitude
+                ));
+            } else {
+                filteredParentStations.add(new Station (
+                        clusteredStations.getFirst().getId(),
+                        stationName,
+                        clusteredStations.getFirst().getLatitude(),
+                        clusteredStations.getFirst().getLongitude()
+                ));
+            }
+        }
+
+        return filteredParentStations;
     }
 }
