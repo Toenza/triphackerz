d9f8a3597115bcf8998844ac36ad8e13becc8c18
weissingerh <hanna.weissinger@oebb.at>
2025-06-24 18:32:48 +0200

    min/max time for max travel time

--- triphackerz-ui/src/app/components/max-travel-time/max-travel-time.ts
+++ triphackerz-ui/src/app/components/max-travel-time/max-travel-time.ts
@@ -13,7 +13,9 @@ import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
 })
 export class MaxTravelTime {
   @Output() maxTravelTimeChanged = new EventEmitter<any>();
-  maxTravelTime: string = "01:00";
+  maxTravelTime: string = "01:00:00";
+  min: string = "00:00:00";
+  max: string = "02:59:00";
 
   onSelectionChanged(selection) {
     this.maxTravelTimeChanged.emit(selection.target.value);
